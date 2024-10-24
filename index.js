a413797e4e38de12d43d7e5c87d95aecdc2cc4ff
Eduardo <tkd13000@gmail.com>
2024-10-24 08:40:37 -0600

    Rules setup

--- index.js
+++ index.js
@@ -1,6 +1,82 @@
+//Variables for buttons
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const rock = document.getElementById("rock");
+
+//Other variables
+const winLable = document.getElementById("win");
+const buttonPlayAgainLable = document.getElementById("buttonPlayAgain");
 
 
+let computerSelection;
+
+//Calls the rules.html file
 function send()
 {
     window.location.href = "./rules.html";
+}
+
+//Events for buttons
+//1 is for paper, 2 is for scissors and 3 for rock
+paper.addEventListener("click", function(){
+    computerSelection = Math.floor(1 + Math.random() * 3);//Returns a number {1, 2, 3}
+
+    //Possible results user vs computer
+    switch(computerSelection)
+    {
+        case 1:
+            displayResult("MATCH!");//Paper and paper(computer)
+            break;
+        case 2:
+            displayResult("YOU LOST!");//Paper and scissors(computer)
+            break;
+        case 3:
+            displayResult("YOU WIN!");//Paper and rock(computer)
+            break;
+        default:
+    }
+
+});
+
+scissors.addEventListener("click", function() {
+    computerSelection = Math.floor(1 + Math.random() * 3);//Returns a number {1, 2, 3}
+
+    switch(computerSelection)
+    {
+        case 1:
+            displayResult("YOU WIN!");//scissors and paper(computer)
+            break;
+        case 2:
+            displayResult("MATCH!");//scissors and scissors(computer)
+            break;
+        case 3:
+            displayResult("YOU LOST!");//scissors and rock(computer)
+            break;
+        default:
+    }
+});
+
+rock.addEventListener("click", function() {
+    computerSelection = Math.floor(1 + Math.random() * 3);//Returns a number {1, 2, 3}
+
+    switch(computerSelection)
+    {
+        case 1:
+            displayResult("YOU LOST!");//rock and paper(computer)
+            break;
+        case 2:
+            displayResult("YOU WIN!");//rock and scissors(computer)
+            break;
+        case 3:
+            displayResult("MATCH!");//rock and rock(computer)
+            break;
+        default:
+    }
+});
+
+function displayResult(winner)
+{
+    winLable.style.display = "block";
+    winLable.textContent = winner;
+    buttonPlayAgainLable.style.display = "block";
 }
